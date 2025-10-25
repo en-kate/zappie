89714e24468c5124fac7c8ae7b2041d88046f85e
en-kate <katy09031997@gmail.com>
2025-10-25 09:17:24 +0000

    enhypen

--- flutter_labs/lib/main.dart
+++ flutter_labs/lib/main.dart
@@ -7,20 +7,126 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-//добавить виджеты Container, networkimage, circleAvatar,
-//св-ва style у  Text, decoration у Container
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children:[
-          Text('Hello World!'),
-          Text('Hello World!'),
-          Text('Hello World!'),
-          Text('Hello World!'),
-          Text('Hello World!'),
-          ]
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blue.shade100, Colors.purple.shade100],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // Первый текст с стилем
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8,
+                      offset: Offset(2, 2),
+                    ),
+                  ],
+                ),
+                child: Text(
+                  'ENHYPEN Profile',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple.shade800,
+                    fontFamily: 'Arial',
+                  ),
+                ),
+              ),
+
+              // CircleAvatar с NetworkImage
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 3,
+                  ),
+                ),
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                    '',
+                  ),
+                ),
+              ),
+
+              // Контейнер с изображением
+              Container(
+                width: 200,
+                height: 120,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      '',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  border: Border.all(
+                    color: Colors.blue.shade300,
+                    width: 2,
+                  ),
+                ),
+              ),
+
+              // Текст с другим стилем
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.purple.shade50,
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: Colors.purple.shade200,
+                  ),
+                ),
+                child: Text(
+                  'K-pop Boy Group • 7 Members',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.purple.shade700,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+
+              // Еще один контейнер с текстом
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.blue.shade200, Colors.purple.shade200],
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  'Debut: November 30, 2020',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
